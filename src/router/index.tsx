f7722049cd45f267132ec8531697a981c11a0732
wumingdao <wumingdao070@gmail.com>
2025-11-26 00:01:17 +0800

    Add NavBar and router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,60 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
+import AppLayout from "../ui/AppLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: AppLayout,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />,
+      },
+      {
+        path: "home",
+        lazy: () => import("../ui/Home"),
+        children: [
+          { index: true, element: <Navigate to="score" replace /> },
+          {
+            element: <></>,
+            children: [
+              {
+                path: "score",
+                element: <div>Score</div>,
+              },
+              {
+                path: "student",
+                element: <div>Student</div>,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "profile",
+        element: <div>profile</div>,
+      },
+
+      // Auth
+      {
+        path: "auth",
+        children: [
+          {
+            path: "login",
+            element: <div>login</div>,
+          },
+          {
+            path: "register",
+            element: <div>register</div>,
+          },
+        ],
+      },
+
+      // 404 Not found
+      {
+        path: "*",
+        element: <div>404</div>,
+      },
+    ],
   },
 ]);
