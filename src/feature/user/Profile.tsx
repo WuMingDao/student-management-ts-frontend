1c57c4e10c6abd7f6ba0dfb5967dee5a03ea20d5
wumingdao <wumingdao070@gmail.com>
2025-11-26 07:08:00 +0800

    Add change avatar in profile page

--- src/feature/user/Profile.tsx
+++ src/feature/user/Profile.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+
 export const Component = function Profile() {
+  const [avatarUrl, setAvatarUrl] = useState<string>(
+    "https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
+  );
+
+  function handAvatarChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.files) {
+      setAvatarUrl(URL.createObjectURL(event.target.files[0]));
+    }
+  }
+
   return (
     <div className="flex justify-center items-center p-40">
       <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 shadow-2xl shadow-blue-300">
         <fieldset className="fieldset w-full">
           <div className="avatar flex justify-center ">
             <div className="w-24 rounded-full">
-              <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+              <label htmlFor="input-avatar">
+                <img src={avatarUrl} />
+              </label>
             </div>
           </div>
+
+          <input
+            id="input-avatar"
+            type="file"
+            accept="image/*"
+            onChange={handAvatarChange}
+            className="hidden"
+          />
 
           <label className="input">
             wu
