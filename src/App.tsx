1d7e8be89b5274d78afa94ba0b2153eaddcd7023
wumingdao <wumingdao070@gmail.com>
2025-11-25 23:01:25 +0800

    setup router and ui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router";
+import { router } from "./router";
+
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <RouterProvider router={router} />,
     </>
   );
 }
